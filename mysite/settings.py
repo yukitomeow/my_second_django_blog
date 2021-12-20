c9cf3dcd1c7e4c306606628dfcd14b142586d933
Yukimi Otagiri <72503145+yukitomeow@users.noreply.github.com>
2021-12-20 17:21:24 +0900

    Modified setting.py for production enviroment

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,8 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
-PROJECT_ROOT = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'staticfiles')
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
